0ebbce9c45ecdc93c97c7cb278cea6642a742def
soletan <soletan@bd04781c-5f47-4fad-9cd2-72e0b05311b9>
2012-03-12 20:31:58 +0000

    adding integration with syslog adding methods for addressing different log levels, each supporting sprintf like behaviour

--- txf/classes/log.php
+++ txf/classes/log.php
@@ -28,10 +28,145 @@
 namespace de\toxa\txf;
 
 
+/**
+ * Logging
+ * 
+ * @author Thomas Urban <thomas.urban@toxa.de>
+ * 
+ */
+
 class log
 {
+	/**
+	 * mark on whether logging has been prepared already or not
+	 * 
+	 * @var boolean
+	 */
+
+	protected static $prepared = false;
+
+	/**
+	 * Prepares use of logger.
+	 * 
+	 * This method may be called to switch previous name of application. It 
+	 * might be called at any time as it's preventing multiple preparations
+	 * internally.
+	 * 
+	 * @param string $applicationName name of application to show in logs
+	 */
+
+	public static function prepare( $applicationName = null )
+	{
+		if ( !self::$prepared || ( $applicationName !== null && $applicationName !== self::$prepared ) )
+		{
+			openlog( ( $applicationName !== null ) ? "TXF.$applicationName" : 'TXF', LOG_NDELAY+LOG_PID, LOG_USER );
+
+			self::$prepared = ( $applicationName !== null ) ? $applicationName : true; 
+		}
+	}
+
+	/**
+	 * Logs a message for debugging.
+	 * 
+	 * This method takes a variable number of arguments and processes them 
+	 * similarly to sprintf(). The resulting message is logged accordingly and
+	 * returned for further processing.
+	 * 
+	 * @return string formatted message
+	 */
+
 	public static function debug()
 	{
-		
+		static::prepare();
+
+		$args = func_get_args();
+
+		syslog( LOG_DEBUG, $msg = call_user_func_array( 'sprintf', $args ) );
+
+		return $msg;
+	}
+
+	/**
+	 * Logs a message containing notice.
+	 * 
+	 * This method takes a variable number of arguments and processes them 
+	 * similarly to sprintf(). The resulting message is logged accordingly and
+	 * returned for further processing.
+	 * 
+	 * @return string formatted message
+	 */
+
+	public static function notice()
+	{
+		static::prepare();
+
+		$args = func_get_args();
+
+		syslog( LOG_NOTICE, $msg = call_user_func_array( 'sprintf', $args ) );
+
+		return $msg;
+	}
+
+	/**
+	 * Logs a message containing warnings.
+	 * 
+	 * This method takes a variable number of arguments and processes them 
+	 * similarly to sprintf(). The resulting message is logged accordingly and
+	 * returned for further processing.
+	 * 
+	 * @return string formatted message
+	 */
+
+	public static function warning()
+	{
+		static::prepare();
+
+		$args = func_get_args();
+
+		syslog( LOG_WARNING, $msg = call_user_func_array( 'sprintf', $args ) );
+
+		return $msg;
+	}
+
+	/**
+	 * Logs a message containing error.
+	 * 
+	 * This method takes a variable number of arguments and processes them 
+	 * similarly to sprintf(). The resulting message is logged accordingly and
+	 * returned for further processing.
+	 * 
+	 * @return string formatted message
+	 */
+
+	public static function error()
+	{
+		static::prepare();
+
+		$args = func_get_args();
+
+		syslog( LOG_ERR, $msg = call_user_func_array( 'sprintf', $args ) );
+
+		return $msg;
+	}
+
+	/**
+	 * Logs a message containing exceptional state.
+	 * 
+	 * This method takes a variable number of arguments and processes them 
+	 * similarly to sprintf(). The resulting message is logged accordingly and
+	 * returned for further processing.
+	 * 
+	 * @return string formatted message
+	 */
+
+	public static function exception()
+	{
+		static::prepare();
+
+		$args = func_get_args();
+
+		syslog( LOG_ALERT, $msg = call_user_func_array( 'sprintf', $args ) );
+
+		return $msg;
 	}
 }
