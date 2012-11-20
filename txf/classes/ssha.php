e3b0a27a0049827e882197aeb304d0ba434dd79c
soletan <soletan@bd04781c-5f47-4fad-9cd2-72e0b05311b9>
2012-11-20 00:25:53 +0000

    fixing issues in methods of class ssha

--- txf/classes/ssha.php
+++ txf/classes/ssha.php
@@ -3,25 +3,25 @@
 
 /**
  * Copyright 2012 Thomas Urban, toxA IT-Dienstleistungen
- * 
+ *
  * This file is part of TXF, toxA's web application framework.
- * 
- * TXF is free software: you can redistribute it and/or modify it under the 
- * terms of the GNU General Public License as published by the Free Software 
- * Foundation, either version 3 of the License, or (at your option) any later 
+ *
+ * TXF is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
- * TXF is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ *
+ * TXF is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * TXF. If not, see http://www.gnu.org/licenses/.
  *
  * @copyright 2012, Thomas Urban, toxA IT-Dienstleistungen, www.toxa.de
  * @license GNU GPLv3+
  * @version: $Id$
- * 
+ *
  */
 
 
@@ -32,13 +32,13 @@ class ssha
 {
 	/**
 	 * Creates salted hash of provided value.
-	 * 
+	 *
 	 * @param mixed $value data to be hashed, any non-string gets serialized first
 	 * @param string $salt salt to use explicitly, omit for random salt
 	 * @return string salted hash of provided data
 	 */
 
-	public static function get( $value, $salt = null )
+	public static function get( $value, $salt = null, $raw = false )
 	{
 		if ( $salt === null )
 			$salt = sha1( mt_rand() );
@@ -46,19 +46,23 @@ class ssha
 		if ( !is_string( $value ) )
 			$value = serialize( $value );
 
-		return '{SSHA}' . base64_encode( sha1( $value . $salt, true ) . $salt );
+		$hash = sha1( $value . $salt, true ) . $salt;
+
+		return $raw ? '{SSHA}' . base64_encode( $hash ) : $hash;
 	}
 
 	/**
 	 * Extracts salt from provided hash.
-	 * 
+	 *
 	 * @param string $hash salted hash previously returned from a call to ssha::get()
 	 * @return string salt of provided hash
 	 */
 
 	public static function extractSalt( $hash )
 	{
-		return substr( 20, base64_decode( substr( 6, $hash ) ) );
+		if ( substr( $hash, 0, 6 ) === '{SSHA}' )
+			return substr( base64_decode( substr( 6, $hash ) ), 20 );
+
+		return substr( $hash, 20 );
 	}
 }
-
